40e5357ae48b9bb190f8ba982ab02b2068182fed
Alexandra Levina <93614091+Drumlex17@users.noreply.github.com>
2025-10-18 03:04:41 +0300

    изменила тест

--- tests/test_android_app_wikipedia.py
+++ tests/test_android_app_wikipedia.py
@@ -5,19 +5,24 @@ from selene import browser, have, be
 
 def test_getting_started():
     """Поиск Onboarding"""
+    with allure.step("Проверяем наличие модального окна"):
+        try:
+            browser.element((AppiumBy.ACCESSIBILITY_ID, "Ok")).click()
+        except:
+            pass
     try:
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).should(be.visible)
-        with allure.step("Проверка заголовка и кнопки добавления языка на первой странице и переход на следующую"):
+        with allure.step("Проверка onboarding экранов"):
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")).should(have.exact_text("The Free Encyclopedia"))
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/addLanguageButton")).should(have.text("Add or edit languages"))
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
-        with allure.step("Проверка заголовка второй страницы и переход на следующую"):
+
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")).should(have.text("New ways to explore"))
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
-        with allure.step("Проверка заголовка третьей страницы и переход на следующую"):
+
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")).should(have.text("Reading lists with sync"))
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
-        with allure.step("Проверка заголовка четвёртой страницы и нажатие на кнопку 'Get Started'"):
+
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")).should(have.text("Data & Privacy"))
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_done_button")).click()
     except:
